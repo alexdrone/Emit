ac3d007dcfd1472e0508b7c6b21e1432845f6e83
Alex Usbergo <alexusbergo@webrtc.org>
2018-12-27 14:15:30 +0100

    dist

---
+++ bin/Emit.framework/Headers/Emit.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Observable.
+FOUNDATION_EXPORT double EmitVersionNumber;
+
+//! Project version string for Observable.
+FOUNDATION_EXPORT const unsigned char EmitVersionString[];
+
+
